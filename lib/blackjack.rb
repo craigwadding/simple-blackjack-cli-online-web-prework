c502014b5866f4609fa582cba42dc62c822d608a
craig wadding <craig.wadding@gmail.com>
2019-03-17 17:04:43 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,14 +1,16 @@
 def welcome
   # code #welcome here
-  puts ""
+  puts "Welcome to the BlackJack Table"
 end
 
 def deal_card
   # code #deal_card here
+  
 end
 
 def display_card_total
   # code #display_card_total here
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
@@ -17,10 +19,12 @@ end
 
 def get_user_input
   # code #get_user_input here
+  gets 
 end
 
 def end_game
   # code #end_game here
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
